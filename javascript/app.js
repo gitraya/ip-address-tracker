0aec49744dbf342c0018b6cbd0a288eeca66c7ac
gitraya <mail.raya15@gmail.com>
2021-03-30 08:55:08 +0700

    Up

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,11 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const dotenv = require('dotenv/types');
+dotenv.config();
+
+console.log(process.env);
+
 async function req() {
   const res = await fetch(
     'https://geo.ipify.org/api/v1?apiKey=at_sWBcpuEmjOoiahw0jdn9pmebP4Zz7'
